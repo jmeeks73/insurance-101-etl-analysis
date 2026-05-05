3e607930e3c337252468f5c20215bf61ed42dd9e
John Meeks <jpmeeks73@yahoo.com>
2026-05-04 22:41:05 -0500

    Enhance insurance data extraction script
    Added comments and print statements to extract insurance data.

--- python/01_extract_insurance_data.py
+++ python/01_extract_insurance_data.py
@@ -1,1 +1,9 @@
+# Extract Github Python Scripts
 
+#1. Extract the data
+import pandas as pd
+
+df = pd.read_csv(r"C:\Users\jpmee\OneDrive\Desktop\insurance101.csv")
+print(df.head())
+print(df.shape)
+print(df.columns)
